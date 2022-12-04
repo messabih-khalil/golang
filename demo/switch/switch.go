b305e45ab291bb5934043dbef8cfa7768443d7e5
messabih-khalil <msbih.khalil@gmail.com>
2022-12-04 20:36:58 +0100

    go switch

---
+++ demo/switch/switch.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func price() int {
+	return 1
+}
+
+const (
+	Economy    = 0
+	Business   = 1
+	FirstClass = 2
+)
+
+func main() {
+	switch p := price() ; {
+		case p < 2:
+			fmt.Println("cheap item")
+		case p < 10 :
+			fmt.Println("Mid price")
+		default:
+			fmt.Println("expensive item")
+	}
+
+	ticket := Economy
+	
+	switch ticket {
+		case Economy:
+			fmt.Println("Economy Seating")
+		case Business:
+			fmt.Println("Business Seating")
+		case FirstClass:
+			fmt.Println("FirstClass Seating")
+		default:
+			fmt.Println("Other Seating")
+		
+	}
+}
