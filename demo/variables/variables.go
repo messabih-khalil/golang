bea7032ace65d941eaa2a7bf8069e79c755779e8
messabih-khalil <msbih.khalil@gmail.com>
2022-12-04 15:25:52 +0100

    go variables

---
+++ demo/variables/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// string var
+	var myName string = "aldin"
+	fmt.Println("my name is : " , myName)
+
+	// create and asigne 
+	username := "tars_aldin"
+	fmt.Println("username : " , username)
+
+	// unsigned variable 
+	var sum int = 0
+	sum = 5
+	fmt.Println(sum)
+
+}
