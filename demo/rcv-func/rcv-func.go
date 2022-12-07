2cb45e2e282ae1974f8a40d96e50f44e10d85d8d
messabih-khalil <msbih.khalil@gmail.com>
2022-12-07 19:49:09 +0100

    go receiver function

---
+++ demo/rcv-func/rcv-func.go
@@ -0,0 +1,25 @@
+package main
+
+// type Person struct{
+// 	age int
+// 	name string
+// 	ageInDays int
+// }
+
+// type Peoples struct{
+// 	persons map[string][]Person
+// }
+
+// // calculate age of person with reciver function
+// func (person * Person) calculateAgeInDays() {
+// 	person.ageInDays = person.age * 365
+// }
+
+// // change name reciver
+// func (person *Person) changeName(otherName string){
+// 	person.name = otherName
+// }
+
+func main() {
+	
+}
