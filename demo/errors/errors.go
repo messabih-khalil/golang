265b1375e361210c4c4d33e22d56c5d021c51288
messabih-khalil <msbih.khalil@gmail.com>
2022-12-24 23:54:27 +0100

    handle errors in go

--- demo/errors/errors.go
+++ demo/errors/errors.go
@@ -1,9 +1,34 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type divNumber struct {
+	a , n float64
+}
+
+// error message
+func (d * divNumber) Error() string {
+	return fmt.Sprintf("Cannot divide by zero : %f / %f" , d.a , d.n)
+}
+
+// divide function
+
+func divide(a , b float64) (float64 , error) {
+	if b == 0 {
+		return 0 , &divNumber{a,b}
+	}
+	return a/b , nil
+}
+
+
 
 func main() {
+	result , error := divide(15,2)
+
+	if error != nil {
+		fmt.Println(error)
+		return
+	}
+	fmt.Println(result)
 
 }
