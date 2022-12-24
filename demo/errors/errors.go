073e786620045e35897b7d5a2d4eacd0fe16b3f4
messabih-khalil <msbih.khalil@gmail.com>
2022-12-25 00:27:00 +0100

    handle errors in go

--- demo/errors/errors.go
+++ demo/errors/errors.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type divNumber struct {
 	a , n float64
@@ -20,7 +23,18 @@ func divide(a , b float64) (float64 , error) {
 	return a/b , nil
 }
 
+//  -------------
 
+type Stuff struct{
+	values []int
+}
+
+func (s * Stuff) Get(index int) (int , error) {
+	if index > len(s.values) {
+		return 0 , errors.New(fmt.Sprintf("The index is not diffiend in the array"))
+	}
+	return s.values[index] , nil
+}
 
 func main() {
 	result , error := divide(15,2)
@@ -31,4 +45,14 @@ func main() {
 	}
 	fmt.Println(result)
 
+	stuff := Stuff{
+	}
+	stuff.values = []int{4,5}
+	result1 , err := stuff.Get(10)
+
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+	fmt.Println(result1)
 }
