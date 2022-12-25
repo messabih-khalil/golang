47ff682cf91bbcf14d3f9026f3019c4172c3d17f
messabih-khalil <msbih.khalil@gmail.com>
2022-12-25 01:12:10 +0100

    handle errors in go exercise

--- exercise/errors/errors.go
+++ exercise/errors/errors.go
@@ -14,4 +14,69 @@
 //* Use the `strconv` package from stdlib to convert strings to ints
 //* Use the `errors` package to generate errors
 
-package timeparse
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+
+
+
+type Time struct {
+	hour , minute , secend int
+}
+
+type ErrorMessage struct{
+	value string
+	message string
+}
+
+// handle error message
+
+func (err * ErrorMessage) Error() string{
+	return fmt.Sprintf("%v : %v" , err.value , err.message)
+}
+
+// convert string into hour , minut , secends
+
+func parseTime(time string) (Time , error) {
+	splitString := strings.Split(time, ":")
+	if len(splitString) != 3 {
+		return Time{} , &ErrorMessage{time , "Invalid String"}
+	}else{
+		hour , err := strconv.Atoi(splitString[0]) 
+		if err != nil {
+			return Time{} , &ErrorMessage{time , "Invalid Hour"}
+		}
+		if hour < 0 || hour > 23 {
+			return Time{} , &ErrorMessage{time , "Invalid Hour"}
+		}
+		minute , err := strconv.Atoi(splitString[1]) 
+		if err != nil {
+			return Time{} , &ErrorMessage{time , "Invalid minute"}
+		}
+		if minute < 0 || minute > 59 {
+			return Time{} , &ErrorMessage{time , "Invalid minute"}
+		}
+		secend , err := strconv.Atoi(splitString[2]) 
+		if err != nil {
+			return Time{} , &ErrorMessage{time , "Invalid secend"}
+		}
+		if secend < 0 || secend > 59 {
+			return Time{} , &ErrorMessage{time , "Invalid secend"}
+		}
+		return Time{hour , minute , secend} , nil
+	}
+}
+
+func main(){
+	time , err := parseTime("25:00:45")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(time)
+}
